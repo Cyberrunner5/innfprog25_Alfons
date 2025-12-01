31be37b037418dea89b4bb1ad480ea07eabe5a67
Cyberrunner4 <understandnotwell2@gmail.com>
2025-12-01 17:53:36 +0100

    börjat på mappeexamen

---
+++ eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/register.js
@@ -0,0 +1,19 @@
+//Hur man nu skriver extern javascript och länkar det till en HTML-fil i en annan mapp
+
+let = personal [
+    {
+        namn: "Anders Hansson",
+        anställning: "lektor",
+        kontor: "A201",
+        mejladress: "anders.hansson@gmail.com",
+        kursansvar: "Ekonomisk historia"
+    }
+
+    {
+        namn: 
+        anställning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    }
+]
