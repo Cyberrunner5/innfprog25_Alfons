87ce115e6934a9c18d7c093b46f5a3be4c8343ee
Cyberrunner4 <understandnotwell2@gmail.com>
2025-12-02 21:05:17 +0100

    påfyllt register

--- eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/register.js
+++ eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/register.js
@@ -3,17 +3,72 @@
 let = personal [
     {
         namn: "Anders Hansson",
-        anställning: "lektor",
+        befattning: "Lektor",
         kontor: "A201",
-        mejladress: "anders.hansson@gmail.com",
-        kursansvar: "Ekonomisk historia"
-    }
-
+        mejladress: "anders.hansson@lu.no",
+        kursansvar: "Regnskap"
+    },
+    {
+        namn: "Erik Jansson",
+        befattning: "Lektor",
+        kontor: "A202",
+        mejladress: "erik.jansson@lu.no",
+        kursansvar: "Bedriftsøkonomi 1"
+    },
+    {
+        namn: "Bengt Andersen",
+        befattning: "Rektor",
+        kontor: "B301",
+        mejladress: "bengt.andersen@lu.no",
+        kursansvar: "Ingen kursansvar" //kursiv text i css?
+    },
+    {
+        namn: "Abdakim Faro",
+        befattning: "Lektor",
+        kontor: "A203",
+        mejladress: "abdakim.faro@lu.no",
+        kursansvar: "Interaktionsdesign"
+    },
+    {
+        namn: "Anton Hallegren",
+        befattning: "Vaktmester",
+        kontor: "B101",
+        mejladress: "anton.hallegren@lu.no",
+        kursansvar: "Ingen kursansvar" //kursiv i css?
+    },
     {
         namn: 
-        anställning: 
+        befattning: 
         kontor: 
         mejladress:
         kursansvar: 
-    }
+    },
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },
 ]
