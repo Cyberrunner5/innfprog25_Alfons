5699ad2b754b4e90556d25425e4b442cbc57b007
Cyberrunner4 <understandnotwell2@gmail.com>
2025-12-03 11:48:45 +0100

    forsok o kobble filer

---
+++ eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/registerold.js
@@ -0,0 +1,78 @@
+//Hur man nu skriver extern javascript och länkar det till en HTML-fil i en annan mapp
+
+let = personal [
+    {
+        namn: "Anders Hansson",
+        befattning: "Lektor",
+        kontor: "A201",
+        mejladress: "anders.hansson@lu.no",
+        kursansvar: "Regnskap"
+    },
+    {
+        namn: "Erik Jansson",
+        befattning: "Lektor",
+        kontor: "A202",
+        mejladress: "erik.jansson@lu.no",
+        kursansvar: "Bedriftsøkonomi 1"
+    },
+    {
+        namn: "Bengt Andersen",
+        befattning: "Rektor",
+        kontor: "B301",
+        mejladress: "bengt.andersen@lu.no",
+        kursansvar: "Ingen kursansvar" //kursiv text i css?
+    },
+    {
+        namn: "Abdakim Faro",
+        befattning: "Lektor",
+        kontor: "A203",
+        mejladress: "abdakim.faro@lu.no",
+        kursansvar: "Interaktionsdesign"
+    },
+    {
+        namn: "Anton Hallegren",
+        befattning: "Vaktmester",
+        kontor: "B101",
+        mejladress: "anton.hallegren@lu.no",
+        kursansvar: "Ingen kursansvar" //kursiv i css?
+    }
+]
+
+console.log(personal)
+
+    /*
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },
+    {
+        namn: 
+        befattning: 
+        kontor: 
+        mejladress:
+        kursansvar: 
+    },*/
