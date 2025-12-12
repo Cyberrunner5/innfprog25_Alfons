0eeb674b6805259d7e3bfccfc2498fa8c1220cb3
Cyberrunner4 <understandnotwell2@gmail.com>
2025-12-12 23:44:32 +0100

    halvt funktion 2

--- eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/actions.js
+++ eksamen_innfprog2025/eksamen_innfprog2025/oppgave1/actions.js
@@ -7,14 +7,27 @@ function kortskapare(befattningslista[0]){
 }
 */
 //Funktion 1
-function(indexbefattningslistavimåstefåfrånmapkanske){
+function skrivutkort(indexbefattningslistavimåstefåfrånmapkanske) {
 //För anställda med kursansvar
-if(personal.befattning == befattningslista[1] || befattningslista[2]) {
-document.getElementById("registerlista").innerHTML = `<li> ${personal[0].fornamn} ${personal[0].efternamn} </li> <li> Stilling: ${personal[0].befattning} </li> <li> Kontor: ${personal[0].kontor} </li> <li> E-post: ${personal[0].mejladress} </li> <li> Kursansvar: ${personal[0].kursansvar} </li>` 
+    if(personal.befattning == befattningslista[1] || befattningslista[2]) {
+        //return
+        document.getElementById("registerlista").innerHTML = `<li> ${personal[0].fornamn} ${personal[0].efternamn} </li> <li> Stilling: ${personal[0].befattning} </li> <li> Kontor: ${personal[0].kontor} </li> <li> E-post: ${personal[0].mejladress} </li> <li> Kursansvar: ${personal[0].kursansvar} </li>` 
+        //För anställda utan kursansvar
+    } else {
+        document.getElementById("registerlista").innerHTML = `<li> ${personal[0].fornamn} ${personal[0].efternamn} </li> <li> Stilling: ${personal[0].befattning} </li> <li> Kontor: ${personal[0].kontor} </li> <li> E-post: ${personal[0].mejladress} </li> <li> ${personal[0].kursansvar} </li>` 
+    }
 }
-//För anställda utan kursansvar
-document.getElementById("registerlista").innerHTML = `<li> ${personal[0].fornamn} ${personal[0].efternamn} </li> <li> Stilling: ${personal[0].befattning} </li> <li> Kontor: ${personal[0].kontor} </li> <li> E-post: ${personal[0].mejladress} </li> <li> ${personal[0].kursansvar} </li>` 
-}
+
+skrivutkort()
+
+let utskrift = ""
+
+personal.map(
+    (item) => {utskrift += `<li> ${personal[0].fornamn} ${personal[0].efternamn} </li> <li> Stilling: ${personal[0].befattning} </li> <li> Kontor: ${personal[0].kontor} </li> <li> E-post: ${personal[0].mejladress} </li> <li> Kursansvar: ${personal[0].kursansvar} </li>` }
+)
+
+document.getElementById("registerlista").innerHTML = utskrift
+
 
 
 //[här behöver det vara en variabel]
